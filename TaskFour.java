998cf8314e5ae3a71df2d7a52b73c31f6b2fa4ac
idowu moses <dynamopraize5050@gmail.com>
2024-12-07 11:21:25 +0100

    first commit

---
+++ TaskFour.java
@@ -0,0 +1,19 @@
+	public class TaskFour{
+	public static void main(String[] args){
+
+
+int count = 4;
+
+
+System.out.println("\nMultiples Of Four: ");
+
+while (count < 10){
+
+	System.out.print(count + " ");
+
+	count = count + 4;
+
+}
+	}
+
+}
