ea301a7e1cf9c087df8dec970b79395f044568f5
idowu moses <dynamopraize5050@gmail.com>
2024-12-07 11:29:11 +0100

    first commit

---
+++ TaskSeven.java
@@ -0,0 +1,20 @@
+public class TaskFive{
+	public static void main(String[] args){
+
+int count = 0;
+int counter = 4;
+
+for (count = 0; count < 5; count++){
+System.out.printf("4 ");
+	
+}
+
+while (counter < 9){
+System.out.printf("8 ");
+	counter = counter + 1;
+
+}
+
+}
+
+}
