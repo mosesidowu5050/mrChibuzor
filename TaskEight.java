35c65d47ed2ab8931d43a0bd378b84e2082ffeb1
idowu moses <dynamopraize5050@gmail.com>
2024-12-07 11:30:13 +0100

    first commit

---
+++ TaskEight.java
@@ -0,0 +1,14 @@
+	public class TaskEight{
+	public static void main(String[] args){
+
+int first = 1368;
+int second = 2728;
+
+int sum = first + second;
+
+System.out.println("\nSum the pair: " + sum);
+
+
+
+}
+}
