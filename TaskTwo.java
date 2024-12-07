6c7cf7c25241c57ec5c714687949c2f046ac698b
idowu moses <dynamopraize5050@gmail.com>
2024-12-07 11:13:50 +0100

    first commit

---
+++ TaskTwo.java
@@ -0,0 +1,17 @@
+	public class TaskTwo{
+	public static void main(String[] args){
+
+int count = 1;
+int counter = 2;
+
+
+ System.out.println("\nEven Numbers (count from 1 to 10):");
+
+while (counter <= 11){
+	System.out.println(counter);
+	counter = counter + 2;
+}
+
+}
+
+}
