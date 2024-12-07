884a56068ed245e0007d69b8931c3e1ba7e7086e
idowu moses <dynamopraize5050@gmail.com>
2024-12-07 11:19:38 +0100

    first commit

---
+++ TaskThree.java
@@ -0,0 +1,18 @@
+	public class TaskThree{
+	public static void main(String[] args){
+
+int counts = 1;
+
+
+System.out.println("\nOdd Numbers Loop (count from 1 to 10):");
+
+while (counts < 10){
+
+	System.out.println(counts);
+
+	counts = counts + 2;
+}
+
+}
+
+}
