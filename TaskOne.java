d0156fb57729366f211012d0e646b876e993a7ac
idowu moses <dynamopraize5050@gmail.com>
2024-12-07 10:51:27 +0100

    first commit

---
+++ TaskOne.java
@@ -0,0 +1,16 @@
+	public class TaskOne{
+	public static void main(String[] args){
+
+int count = 1;
+
+System.out.println("\nFirst Loop (count from 1 to 10):");
+
+for (count = 1; count <= 10; count++){
+
+	System.out.println(count);
+		
+
+		}
+
+	}
+}
