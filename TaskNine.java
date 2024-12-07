f5f116a43b3612658353ebddb6861d78473de88d
idowu moses <dynamopraize5050@gmail.com>
2024-12-07 11:31:47 +0100

    first commit

---
+++ TaskNine.java
@@ -0,0 +1,16 @@
+
+	public class TaskNine{
+	public static void main(String[] args){
+
+int first = 1364;
+int second = 2728;
+
+int sum = first + second;
+
+ double squareRoot = Math.sqrt(sum); 
+System.out.printf("Squre root of %d: %f%n", sum, squareRoot);
+
+
+}
+
+}
