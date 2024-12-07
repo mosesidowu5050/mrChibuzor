6cd7a89b5f6e7c9a06c14b25ae31843b12b77ac3
idowu moses <dynamopraize5050@gmail.com>
2024-12-07 11:25:49 +0100

    first commit

---
+++ TaskSix.java
@@ -0,0 +1,18 @@
+public class TaskSix{
+	public static void main(String[] args){
+
+int count = 4;
+
+while (count < 10){
+	System.out.print(count*1 + " ");
+	System.out.print(count*4 + " ");
+	System.out.print(count*16 + " ");
+	System.out.print(count*64 + " ");
+	System.out.print(count*256 + " ");
+
+	count = count + 4;
+}
+
+}
+
+}
